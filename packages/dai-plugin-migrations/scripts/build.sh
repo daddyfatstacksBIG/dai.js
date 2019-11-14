e84ae1fa4d64b5859213da8b2cef194d04a7d7e4
Restyled.io <commits@restyled.io>
2019-11-14 02:53:07 +0000

    Restyled by shellharden

--- packages/dai-plugin-migrations/scripts/build.sh
+++ packages/dai-plugin-migrations/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(basename $(pwd)) != "dai-plugin-migrations" ]; then
+if [ "$(basename "$PWD")" != "dai-plugin-migrations" ]; then
   echo "This script must be run from the dai-plugin-migrations directory."
   exit
 fi
