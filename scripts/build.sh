86cddc87e0fd77a55549531e409ec2edb6fa555f
Restyled.io <commits@restyled.io>
2020-02-21 12:22:36 +0000

    Restyled by shellharden

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $(basename $(dirname $(pwd))) != "packages" ]; then
+if [ "$(basename "$(dirname "$PWD")")" != "packages" ]; then
   echo "This script must be run from a directory under packages/."
 fi
 
-PACKAGE=$(basename $(pwd))
+PACKAGE=$(basename "$PWD")
 echo "Building $PACKAGE..."
 
 rm -rf dist/*
